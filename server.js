a44217b023395ddb0a9291e1b5245ba6258f24e4
e-leal <leal.elyssa03@gmail.com>
2020-08-02 23:55:31 -0500

    added get api/notes call and ereferenced db.json data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { notes } = require('./db/db');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,6 +16,11 @@ app.get('/', (req, res) => {
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
   });
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    return res.json(results);
+})
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
